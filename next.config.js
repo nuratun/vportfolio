85cb372439f6dcaf8ad1c7f6685f1e9c5dee27c9
Noora Chahine <nuratun@users.noreply.github.com>
2021-11-27 13:16:54 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,2 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
-
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/hello',
-        destination: 'https://vercel.com ',
-        permanent: true,
-      },
-    ]
-  },
-}

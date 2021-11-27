dba2376ded555b256f7bf63944d4217544ca51d1
Noora Chahine <nuratun@users.noreply.github.com>
2021-11-27 13:15:50 +0200

    Added redirect

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/hello',
+        destination: 'https://vercel.com ',
+        permanent: true,
+      },
+    ]
+  },
+}

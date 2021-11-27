8e622a10c74f43642220204e1845c70097a6f8d2
Noora Chahine <nuratun@users.noreply.github.com>
2021-11-27 11:58:40 +0200

    Updated site URL and name

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Noora Chahine',
+    site_url: 'https://vportfolio.vercel.app',
+    feed_url: 'https://vportfolio.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
